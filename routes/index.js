611c01feab0753a1e4dc19fd18d51fb298747227
aygun <aygunselvi@hotmail.de>
2019-12-22 17:16:59 +0100

    node js

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/lo', function (req,res) {
+  res.render("../views/London.html");
+});
+router.get('/test', function (req,res) {
+    res.render("../views/test.html");
+});
+
+router.get('/londonJSON',function (req,res) {
+    console.log(__dirname)
+    let rawdata = fs.readFileSync(__dirname+'/../views/London.json');
+    let jsonData = JSON.parse(rawdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(jsonData));
+})
+
+router.get('/London.json', function(res){
+  var json = $.getJSON('London.json', function(json) {
+    console.log(json)
+  })
+});
+
+router.get('/data/londonBoroughs.json', function(res){
+    res.send({"data":1})
+});
+
+module.exports = router;
