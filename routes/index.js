228b295eb9d4aa52bcf06d3f0802971c8eebdd12
baris2 <ceralest55@gmail.com>
2020-01-11 13:33:36 +0100

    Heatmap works I guess

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ router.get('/test', function (req,res) {
     res.render("../views/test.html");
 });
 
+router.get('/map', function (req,res) {
+    res.render("../views/test3.html");
+});
+
 router.get('/londonJSON',function (req,res) {
     let rawdata = fs.readFileSync(__dirname+'/../views/London.json');
     let jsonData = JSON.parse(rawdata);
@@ -36,6 +40,12 @@ router.get('/data/annualEarning.json', function(req,res){
 });
 router.get('/data/ethnicGroups.json', function(req,res){
     let rawdata = fs.readFileSync(__dirname+'/../data/ethnicGroups.json');
+    let jsonData = JSON.parse(rawdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(jsonData));
+});
+router.get('/data/londonBoroughs.json', function(req,res){
+    let rawdata = fs.readFileSync(__dirname+'/../data/londonBoroughs.json');
     let jsonData = JSON.parse(rawdata);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(jsonData));
